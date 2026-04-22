86b4cae741db40909219f9fa46942c2932eaf4b2
DeathHound <acraig176@hotmail.com>
2026-04-22 19:48:06 +0100

    Match targimpl.c

--- configure.py
+++ configure.py
@@ -343,6 +343,7 @@ config.libs = [
             Object(MatchingFor(), "PowerPC_EABI_Support/MetroTRK/dolphin_trk.c"),
             Object(MatchingFor("SALP4Q"), "PowerPC_EABI_Support/MetroTRK/main_TRK.c"),
             Object(MatchingFor("SALP4Q"), "PowerPC_EABI_Support/MetroTRK/mainloop.c"),
+            Object(MatchingFor(), "PowerPC_EABI_Support/MetroTRK/targimpl.c"),
         ]
     },
     {
---
+++ src/PowerPC_EABI_Support/MetroTRK/dstypes.h
@@ -0,0 +1,16 @@
+#ifndef METROTRK_DSTYPES
+    #define METROTRK_DSTYPES
+
+    #include "types.h" // IWYU pragma: keep
+
+    #ifdef __cplusplus
+        extern "C" {
+    #endif
+
+    typedef unsigned char u128[16];
+
+    #ifdef __cplusplus
+        }
+    #endif
+
+#endif
