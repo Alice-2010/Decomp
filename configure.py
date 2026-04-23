8b7f631166d992bd5dbe99ab892c59c619be2927
DeathHound <acraig176@hotmail.com>
2026-04-23 09:46:05 +0100

    Partial match nubinit.c

--- configure.py
+++ configure.py
@@ -346,6 +346,7 @@ config.libs = [
             Object(MatchingFor("SALP4Q"), "PowerPC_EABI_Support/MetroTRK/mainloop.c"),
             Object(MatchingFor(), "PowerPC_EABI_Support/MetroTRK/mpc_7xx_603e.c"),
             Object(MatchingFor(), "PowerPC_EABI_Support/MetroTRK/nubevent.c"),
+            Object(MatchingFor(), "PowerPC_EABI_Support/MetroTRK/nubinit.c"),
             Object(MatchingFor(), "PowerPC_EABI_Support/MetroTRK/targimpl.c"),
         ]
     },
---
+++ src/PowerPC_EABI_Support/MetroTRK/nubinit.c
@@ -0,0 +1,65 @@
+#include "PowerPC_EABI_Support/MetroTRK/trk.h"
+
+BOOL gTRKBigEndian;
+
+DSError TRKInitializeNub(void)
+{
+    DSError ret;
+    DSError uartErr;
+
+    ret = TRKInitializeEndian();
+
+    if (ret == DS_NoError)
+        TRKInitializeEventQueue();
+    if (ret == DS_NoError) {
+        ret = TRKInitializeMessageBuffers();
+        InitializeProgramEndTrap();
+    }
+    if (ret == DS_NoError)
+        ret = TRKInitializeSerialHandler();
+    if (ret == DS_NoError)
+        ret = TRKInitializeTarget();
+
+    if (ret == DS_NoError) {
+        uartErr = TRKInitializeIntDrivenUART(0x0000e100, 1, 0, (volatile u8**)&gTRKInputPendingPtr);
+        TRKTargetSetInputPendingPtr(gTRKInputPendingPtr);
+        if (uartErr != DS_NoError) {
+            ret = uartErr;
+        }
+    }
+
+    return ret;
+}
+
+DSError TRKTerminateNub(void)
+{
+    TRKTerminateSerialHandler();
+    return DS_NoError;
+}
+
+void TRKNubWelcome(void)
+{
+    TRK_board_display("MetroTRK for Revolution v0.4");
+    return;
+}
+
+BOOL TRKInitializeEndian(void)
+{
+    u8 bendian[4];
+    BOOL result   = FALSE;
+    gTRKBigEndian = TRUE;
+
+    bendian[0] = 0x12;
+    bendian[1] = 0x34;
+    bendian[2] = 0x56;
+    bendian[3] = 0x78;
+
+    if (*(u32*)bendian == 0x12345678) {
+        gTRKBigEndian = TRUE;
+    } else if (*(u32*)bendian == 0x78563412) {
+        gTRKBigEndian = FALSE;
+    } else {
+        result = TRUE;
+    }
+    return result;
+}
