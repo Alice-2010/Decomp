d83332ad58586830b288137bf8ce7a2279c64ef3
DeathHound <acraig176@hotmail.com>
2026-04-23 11:21:22 +0100

    Partial match serpoll.c

--- configure.py
+++ configure.py
@@ -347,6 +347,7 @@ config.libs = [
             Object(MatchingFor(), "PowerPC_EABI_Support/MetroTRK/mpc_7xx_603e.c"),
             Object(MatchingFor(), "PowerPC_EABI_Support/MetroTRK/nubevent.c"),
             Object(MatchingFor(), "PowerPC_EABI_Support/MetroTRK/nubinit.c"),
+            Object(MatchingFor(), "PowerPC_EABI_Support/MetroTRK/serpoll.c"),
             Object(MatchingFor(), "PowerPC_EABI_Support/MetroTRK/targimpl.c"),
         ]
     },
