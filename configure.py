c4c8afa9ae8e6690241a3cf86715fb83079d35bf
DeathHound <acraig176@hotmail.com>
2026-04-24 19:48:31 +0100

    Partial match msgbuf.c

--- configure.py
+++ configure.py
@@ -349,6 +349,7 @@ config.libs = [
             Object(MatchingFor("SALP4Q"), "PowerPC_EABI_Support/MetroTRK/mainloop.c"),
             Object(MatchingFor(), "PowerPC_EABI_Support/MetroTRK/mem_TRK.c"),
             Object(MatchingFor(), "PowerPC_EABI_Support/MetroTRK/mpc_7xx_603e.c"),
+            Object(MatchingFor(), "PowerPC_EABI_Support/MetroTRK/msgbuf.c"),
             Object(MatchingFor(), "PowerPC_EABI_Support/MetroTRK/nubevent.c"),
             Object(MatchingFor(), "PowerPC_EABI_Support/MetroTRK/nubinit.c"),
             Object(MatchingFor(), "PowerPC_EABI_Support/MetroTRK/serpoll.c"),
